7bcab79d18fed7c4e81eba28f5ebbf5c8c8775a2
Alo Oluwapelumi David <pelumidavid123456@gmail.com>
2022-09-16 14:55:59 +0100

    commit

--- main.go
+++ main.go
@@ -6,15 +6,17 @@ import (
 )
 
 type MyEvent struct {
-	Name string `json:"What's your name?"`
-	Age  int    `json:"How Old are you?"`
+	Name       string `json:"What's your name?"`
+	Age        int    `json:"How Old are you?"`
+	Occupation string `json:"What job do you do?"`
+	Experience string `json:"What Experience do you have?"`
 }
 type MyResponse struct {
 	Message string `json:"Answer"`
 }
 
 func HandleLambdaEvent(event MyEvent) (MyResponse, error) {
-	return MyResponse{Message: fmt.Sprintf("%v is %v years old!", event.Name, event.Age)}, nil
+	return MyResponse{Message: fmt.Sprintf("%v is %v years old!, I work as a/an %v, currently posses above%v", event.Name, event.Age, event.Occupation, event.Experience)}, nil
 }
 
 func main() {
